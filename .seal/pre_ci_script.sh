b029991da44676c0e368bdb37a73ac92d1ad8a06
Lev Pachmanov <lev.p@sealsecurity.io>
2024-04-28 15:24:06 +0300

    specific project

--- .seal/pre_ci_script.sh
+++ .seal/pre_ci_script.sh
@@ -7,7 +7,7 @@ cd /tmp/propdeps-plugin
 
 git clone --depth=1 --branch netty-4.1.31.Final https://github.com/netty/netty.git /tmp/netty
 cd /tmp/netty
-./mvnw install -DskipTests
+./mvnw install -DskipTests --pl bom
 
 git clone --depth=1 --branch jetty-9.4.14.v20181114 https://github.com/jetty/jetty.project.git /tmp/jetty
 cd /tmp/jetty
