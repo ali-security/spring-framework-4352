d901df7b61873b33e5aaf9317be1711243c81d72
Lev Pachmanov <lev.p@sealsecurity.io>
2024-05-02 10:16:22 +0300

    build

--- .seal/pre_ci_script.sh
+++ .seal/pre_ci_script.sh
@@ -4,11 +4,3 @@ set -ex
 git clone --depth 1 --branch v0.0.9 https://github.com/spring-attic/propdeps-plugin.git /tmp/propdeps-plugin
 cd /tmp/propdeps-plugin
 ./gradlew install -x test
-
-git clone --depth=1 --branch netty-4.1.31.Final https://github.com/netty/netty.git /tmp/netty
-cd /tmp/netty
-./mvnw install -DskipTests --pl bom
-
-git clone --depth=1 --branch jetty-9.4.14.v20181114 https://github.com/jetty/jetty.project.git /tmp/jetty
-cd /tmp/jetty
-/root/.m2/wrapper/dists/apache-maven-*-bin/*/apache-maven-*/bin/mvn install --pl bom -DskipTests
